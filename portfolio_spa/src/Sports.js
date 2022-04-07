a564c25481ffb79e120c0af3033442a5ac53e7be
unknown <awm21@pitt.edu>
2022-04-07 14:42:44 -0400

    fixed formatting on Sports page

---
+++ portfolio_spa/src/Sports.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Sports(){
+    return(
+        <div>
+            <card className="card">
+                <h3>Sports</h3>
+            </card>
+        </div>
+    )
+}
+export default Sports;
